6897ab5d227016b41a779e2e94c67815b36c5b2a
JuliuszR <jraczkowski@wp.pl>
2025-06-07 20:55:01 +0200

    First commit

---
+++ Cwiczenia12PD/Cwiczenia12PD/Controllers/TripsController.cs
@@ -0,0 +1,42 @@
+﻿using Cwiczenia12PD.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cwiczenia12PD.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TripsController : ControllerBase
+{
+    private readonly ApbdContext _context;
+
+    public TripsController(ApbdContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetTrips()
+    {
+        var trips = await _context.Trips
+            .OrderByDescending(t => t.DateFrom)
+            .Select(t => new
+            {
+                t.Name,
+                t.Description,
+                t.DateFrom,
+                t.DateTo,
+                t.MaxPeople,
+                Countries = t.IdCountries.Select(c => new { c.Name }),
+                Clients = t.ClientTrips.Select(ct => new
+                {
+                    ct.IdClientNavigation.FirstName,
+                    ct.IdClientNavigation.LastName
+                })
+            })
+            .ToListAsync();
+
+        return Ok(trips);
+    }
+
+}
---
+++ Cwiczenia12PD/Cwiczenia12PD/Program.cs
@@ -0,0 +1,32 @@
+
+
+using Cwiczenia12PD.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddAuthorization();
+builder.Services.AddDbContext<ApbdContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
